69abf2c530732cdbc7e3d582707f71301a0acb25
strattt <valera32dll@gmail.com>
2014-02-14 08:23:09 +0400

    add

---
+++ src/Phone.java
@@ -0,0 +1,31 @@
+/**
+ * User: che
+ * Date: 14.02.14
+ * Time: 8:09
+ */
+public class Phone {
+    public static void main(String args[]) {
+        String numbers[][] = {
+                {"Tom", "111-111"},
+                {"Mary", "222-222"},
+                {"Jon", "333-333"},
+                {"Rachel", "444-444"}
+        };
+
+        int i;
+
+        if (args.length != 1)
+            System.out.println("Usage: java  phone <name>");
+        else {
+            for (i = 0; i < numbers.length; i++) {
+                if(numbers[i][0].equals(args[0])) {
+                    System.out.println(numbers[i][0] + " : " + numbers[i][1]);
+                    break;
+                }
+            }
+
+            if (i == numbers.length)
+                System.out.println("Name not found.");
+        }
+    }
+}
