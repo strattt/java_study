54f2bbcf2d89e07489abccb769583321b85492b2
strattt <valera32dll@gmail.com>
2014-02-24 08:56:18 +0400

    add

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -5,15 +5,22 @@
  */
 public class ArrayTest {
     public static void main(String args[]) {
-        double d[];
-        d = new double[10];
 
-        int n[] = {1, 2, 3, 4, 5};
+
+        //среднеарефмитическое десяти чисел
+        double nums[] = {1, 2, 3, 4, 5, 100, 7, 856, 900, 10};
+        double result = 0;
+        for (int i = 0; i < nums.length; i++ ) {
+            double a = result + nums[i];
+            result = a;
+            System.out.println(a);
+        }
+
+        double b = result / nums.length;
+        System.out.println(b);
+
 
 
 
     }
-
-
-
 }
--- src/Bubble.java
+++ src/Bubble.java
@@ -5,10 +5,11 @@
  */
 public class Bubble {
     public static void main(String args[]) {
+
+//сортировка массива цифр
         int nums[] = {99, -10, 100132, 18, -978, 5623, 463, -9, 287, 49};
         int a, b, t;
         int size;
-
         size = 10;//число элементов для сортировки
 
         //отображение исходного массива
@@ -30,12 +31,36 @@ public class Bubble {
 
             }
 
-
         //отображение отсортированного массива
         System.out.println("Sorted array is: ");
         for (int i = 0; i < size; i++)
             System.out.println(" " + nums[i]);
         System.out.println();
+
+
+//сортировка массива строк
+        String str[] = {"Javacode", "I", "Like"};
+        int c, d;
+        String e;
+
+
+        for (c = 1; c < str.length; c++)
+            for (d = str.length -1; d >= c; d--)  {
+                String st = str[d];
+                String st1 = str[d -1];
+                if (st.length() < st1.length()) {
+                    //если треуемы порядок следования не соблюдается, элементы меняются местами
+                    e = str[d -1];
+                    str[d -1] = str[d];
+                    str[d] = e;
+                }
+            }
+
+        System.out.println("Sorted array is: ");
+        for (int i = 0; i < str.length; i++)
+            System.out.println(" " + str[i]);
+        System.out.println();
+
 
     }
 }
